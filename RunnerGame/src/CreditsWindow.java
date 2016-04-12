131e07e1035f23a1d3dfc69b617b2c3c65469ad4
Milena Sapunova <milena.sapunova@gmail.com>
2016-04-13 01:49:46 +0300

    deleted some old stuff

--- RunnerGame/src/CreditsWindow.java
+++ RunnerGame/src/CreditsWindow.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
